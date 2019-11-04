ddafb26cd6e27800f3148143afba917e7afb4e2f
kontopoulos <kontopoulos@hua.gr>
2019-11-04 11:07:11 +0200

    -

--- src/main/scala/gr/hua/glasseas/VoyageSparkApp.scala
+++ src/main/scala/gr/hua/glasseas/VoyageSparkApp.scala
@@ -23,7 +23,6 @@ object VoyageSparkApp {
 
     val gc = new GlasseasContext
     val data = gc.readData(filename,sc).filter(_.shipType.contains(shipType))
-    println("Dataset converted to RDD...")
     data.keyBy(_.id).partitionBy(new BalancedPartitioner(numPartitions,data.map(_.id))).glom().flatMap{
       positions =>
         val voyages: ArrayBuffer[Voyage] = ArrayBuffer()
@@ -32,50 +31,45 @@ object VoyageSparkApp {
             var previousPort = -1
             var voyageId = UUID.randomUUID().toString
             var voyage: ArrayBuffer[AISPosition] = ArrayBuffer()
-            var previousPosition = AISPosition(-1, -1, 0.0, 0.0, 0.0, 0.0, 0.0, 0L, "", "", "", "", "")
-            vesselPositions.sortBy(_._2.timestamp).map(_._2).foreach{ // loop through every position to segment the trajectory into voyages
-              pos =>
+            val positions = vesselPositions.sortBy(_._2.timestamp)
+            var previousPosition = positions.head._2
+
+            def splitVoyage(duration: Int, pos: AISPosition): Unit = {
+              if (pos.seconds - previousPosition.seconds >= duration) {
+                beginNewVoyage(-1)
+              }
+            }
+
+            def beginNewVoyage(newWaypoint: Int): Unit = {
+              val itinerary = s"${previousPort}_to_$newWaypoint"
+              val completedVoyage = Voyage(voyageId,itinerary,voyage)
+              voyages.append(completedVoyage)
+              voyage = ArrayBuffer() // begin new voyage
+              previousPort = newWaypoint
+              voyageId = UUID.randomUUID().toString // new distinct voyage identifier
+            }
+
+            positions.foreach{ // loop through every position to segment the trajectory into voyages
+              case (mmsi,pos) =>
                 LocalDatabase.getEnclosingWaypoint(GeoPoint(pos.longitude,pos.latitude)) match {
                   case Some(port) =>
-                    if (previousPort != port._2 && previousPosition.nonEmpty()) { // different port id which means that the voyage ended
-                      val itinerary = s"${previousPort}_to_${port._2}"
+                    if (previousPort != port._2) { // different port id which means that the voyage ended
                       voyage.append(pos)
-                      val completedVoyage = geotools.Voyage(voyageId,itinerary,voyage)
-                      voyages.append(completedVoyage)
-                      voyage = ArrayBuffer() // begin new voyage
-                      previousPort = port._2 // store the starting port of the new voyage
-                      voyageId = UUID.randomUUID().toString // new distinct voyage identifier
+                      beginNewVoyage(port._2)
                     }
                     else {
-                      if (pos.seconds - previousPosition.seconds >= 86400 && previousPosition.nonEmpty()) { // equal to or more than a day
-                        val itinerary = s"${previousPort}_to_-1"
-                        val completedVoyage = geotools.Voyage(voyageId,itinerary,voyage)
-                        voyages.append(completedVoyage)
-                        voyage = ArrayBuffer() // begin new voyage
-                        previousPort = -1 // invalid previous port
-                        voyageId = UUID.randomUUID().toString // new distinct voyage identifier
-                      }
+                      splitVoyage(86400,pos) // equal to or more than a day
                       voyage.append(pos)
                     }
                   case None =>
-                    if (pos.seconds - previousPosition.seconds >= 86400 && previousPosition.nonEmpty()) { // equal to or more than a day
-                      val itinerary = s"${previousPort}_to_-1"
-                      val completedVoyage = geotools.Voyage(voyageId,itinerary,voyage)
-                      voyages.append(completedVoyage)
-                      voyage = ArrayBuffer() // begin new voyage
-                      previousPort = -1 // invalid previous port
-                      voyageId = UUID.randomUUID().toString // new distinct voyage identifier
-                    }
+                    splitVoyage(86400,pos) // equal to or more than a day
                     voyage.append(pos) // no port is found, which means that the vessel is still travelling at the open sea and continues its voyage
                 }
                 previousPosition = pos
             }
         }
         voyages
-    }.filter(v => !v.itinerary.contains("-1")).saveAsTextFile(s"$shipType-trips")
-
-
-
+    }.filter(v => !v.itinerary.contains("-1")).saveAsTextFile(s"$filename-$shipType-voyages")
   }
 
 }
